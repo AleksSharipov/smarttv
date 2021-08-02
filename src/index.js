9009c2c077481ab67d62730fa4f03dcdc328cb17
Aleksandr Sharipov <sharipov.sasha.spb@yandex.ru>
2021-08-02 03:57:39 +0300

    init

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import App from './components/App/App';
 import './index.css';
 
 ReactDOM.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>,
-  ,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
